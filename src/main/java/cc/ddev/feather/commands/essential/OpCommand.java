abe7dcf2bcf7f18fa50ae3dd394dad7cf8df0bba
xkaasplakje <57845320+duranaaron@users.noreply.github.com>
2022-12-29 00:50:27 +0100

    Cleaned imports

--- src/main/java/cc/ddev/feather/commands/essential/OpCommand.java
+++ src/main/java/cc/ddev/feather/commands/essential/OpCommand.java
@@ -7,12 +7,10 @@ import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.arguments.minecraft.ArgumentEntity;
 import net.minestom.server.entity.Entity;
-import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
 import net.minestom.server.utils.entity.EntityFinder;
 
 import java.util.List;
-import java.util.Locale;
 
 public class OpCommand extends Command {
 
