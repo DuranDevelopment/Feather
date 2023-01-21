b2b582e058c43aee7a0461c76f0d94dc654aee23
xkaasplakje <57845320+duranaaron@users.noreply.github.com>
2023-01-21 21:15:30 +0100

    Changed styling of item names in GUI

--- src/main/java/cc/ddev/feather/utils/ChatUtils.java
+++ src/main/java/cc/ddev/feather/utils/ChatUtils.java
@@ -1,10 +1,15 @@
 package cc.ddev.feather.utils;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class ChatUtils {
     public static Component translateMiniMessage(String string) {
-        return MiniMessage.miniMessage().deserialize(string);
+        return MiniMessage.miniMessage().deserialize(string).decoration(TextDecoration.ITALIC, false);
+    }
+
+    public static Component translateMiniMessage(String string, boolean italic) {
+        return MiniMessage.miniMessage().deserialize(string).decoration(TextDecoration.ITALIC, italic);
     }
 }
